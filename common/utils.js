8fdc7bbb0cd74f9dc28282fe0bc18dfa9f0a6f90
Veronica Mihai <mihaiveronica@ymail.com>
2020-05-02 17:34:07 +0300

    add utils array comparison functions

---
+++ common/utils.js
@@ -0,0 +1,24 @@
+const isArrayEqual = (arr1, arr2) => {
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+const isArrayOfObjectsEqual = (arr1, arr2) => {
+    for (let i = 0; i < arr1.length; i++) {
+        if (JSON.stringify(arr1[i]) !== JSON.stringify(arr2[i])) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+module.exports = {
+    isArrayEqual,
+    isArrayOfObjectsEqual
+}

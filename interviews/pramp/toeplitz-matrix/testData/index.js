33db1cac24518a2503723b6e41c2d5b7c9313a3b
Veronica Mihai <mihaiveronica@ymail.com>
2020-05-08 15:03:16 +0300

    toeplitz-matrix => add additional test cases

--- interviews/pramp/toeplitz-matrix/testData/index.js
+++ interviews/pramp/toeplitz-matrix/testData/index.js
@@ -19,12 +19,23 @@ const testMatrixes = [
         label: 'non-toeplitz-matrix1'
     },
     {
+        params: [[1, 2, 3, 4],
+        [5, 1, 2, 3],
+        [6, 4, 1, 2]],
+        expectedResult: false,
+        label: 'non-toeplitz-matrix2'
+    },
+    {
         params: [[3, 7, 0, 9, 8],
         [5, 3, 7, 0, 9],
         [6, 5, 3, 7, 0],
         [4, 6, 5, 3, 7]],
         expectedResult: true,
         label: 'toeplitz-matrix2'
+    }, {
+        params: [[4, 0], [9, 4]],
+        expectedResult: true,
+        label: 'toeplitz-matrix3'
     },
 ];
 

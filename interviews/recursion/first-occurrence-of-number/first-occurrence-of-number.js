e899a32bee6e19e9522d6731be270bc7b9f32556
Veronica Mihai <mihaiveronica@ymail.com>
2020-05-08 09:35:22 +0300

    recursion: first occurrence of number => initial setup with tests and logging

---
+++ interviews/recursion/first-occurrence-of-number/first-occurrence-of-number.js
@@ -0,0 +1,55 @@
+const colors = require('colors');
+const common = require('../../../common/utils.js');
+const testCases = require('./testData/index.js');
+
+/* 
+ *  Arguments:
+ *  arr: Array<int>
+ *  startIndex: int
+ *  value: int
+ * 
+ * returns index of first occurrence of value in arr
+ * eg. 
+ * const arr = [1, 2, 3, 4, 5];
+ * startIndex = 0
+ * value 4
+ * console.log(findIndex(arr, startIndex, value))
+ * return '3'
+*/
+
+function findIndex(arr, startIndex, value) {
+    return startIndex;
+}
+
+function assert(arr, startIndex, value, result, expected) {
+    const resultMsg = `Input arr is ${arr}, startIndex is ${startIndex} and value is ${value}. The expected number of vowels value is ${expected}`;
+    if (result === expected) {
+        console.log(resultMsg.green)
+        return true;
+    } else {
+        console.error(`${resultMsg}, found ${result}`.brightRed);
+        return false;
+    }
+}
+
+function printTestResult(solutionFunc) {
+    let count = 0;
+    const { testArrays } = testCases;
+
+    ((testData) => {
+        testData.forEach(function loopUnitTests({ arr, startIndex, value, expectedResult, label = '' }, index) {
+            console.log(`${index + 1}. ${label}`.yellow);
+
+            const isCorrect = assert(arr, startIndex, value, solutionFunc(arr, startIndex, value), expectedResult);
+
+            if (!isCorrect) {
+                count += 1;
+            }
+        });
+
+        console.error(`You have ${count}/${testData.length} failling tests`.brightRed);
+        console.error(`You have ${testData.length - count}/${testData.length} passing tests. Congrats!!!`.green);
+    })(testArrays);
+}
+
+printTestResult(findIndex);
---
+++ interviews/recursion/first-occurrence-of-number/testData/index.js
@@ -0,0 +1,41 @@
+const testArrays = [
+    {
+        arr: [],
+        startIndex: 0,
+        value: 1,
+        expectedResult: -1,
+        label: 'empty-array'
+    },
+    {
+        arr: [1],
+        startIndex: 0,
+        value: 2,
+        expectedResult: -1,
+        label: 'not-found-value1'
+    },
+    {
+        arr: [1, 2, 3, 4],
+        startIndex: 1,
+        value: 1,
+        expectedResult: -1,
+        label: 'not-found-value2'
+    },
+    {
+        arr: [1, 2, 3, 4, 5],
+        startIndex: 1,
+        value: 4,
+        expectedResult: 3,
+        label: 'found-value1'
+    },
+    {
+        arr: [1, 2, 3, 4, 5],
+        startIndex: 3,
+        value: 5,
+        expectedResult: 4,
+        label: 'found-value2'
+    },
+];
+
+module.exports = {
+    testArrays,
+};

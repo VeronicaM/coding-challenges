01ccda600c97e69c8ef4aea9170e5d8f77fbd271
Veronica Mihai <veronica.mihai90@outlook.com>
2020-05-13 08:34:38 +0300

    recursion: sort tree => move tree class into common and builtTree method into utils

--- common/utils.js
+++ common/utils.js
@@ -1,4 +1,21 @@
 const colors = require('colors');
+const TreeNode = require('./TreeNode.js').TreeNode;
+
+const buildTree = (multiplierFactor = 1) => {
+    const emptyNode = new TreeNode(null, null, null);
+
+    const firstNodeLeftTree = new TreeNode(2 * multiplierFactor, emptyNode, emptyNode);
+    const secondNodeLeftTree = new TreeNode(3 * multiplierFactor, firstNodeLeftTree, emptyNode);
+
+    const leftTree = new TreeNode(4 * multiplierFactor, secondNodeLeftTree, emptyNode)
+
+    const firstNodeRightTree = new TreeNode(6 * multiplierFactor, emptyNode, emptyNode);
+    const secondNodeRightTree = new TreeNode(11 * multiplierFactor, emptyNode, emptyNode);
+
+    const rightTree = new TreeNode(10 * multiplierFactor, firstNodeRightTree, secondNodeRightTree);
+
+    return new TreeNode(5 * multiplierFactor, leftTree, rightTree);
+};
 
 const isArrayEqual = (arr1, arr2) => {
     if (arr1.length !== arr2.length) return false;
@@ -85,5 +102,6 @@ module.exports = {
     isArrayEqual,
     isArrayOfObjectsEqual,
     printTestResult,
-    printMatrix
+    printMatrix,
+    buildTree
 }
